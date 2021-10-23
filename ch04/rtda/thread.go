e154b2bab838235ff2533f841941208b927a9ba8
李毅恒 <834805011@qq.com>
2021-10-23 10:42:11 +0800

    运行时数据区

---
+++ ch04/rtda/thread.go
@@ -0,0 +1,32 @@
+package rtda
+
+type Thread struct {
+	pc    int
+	stack *Stack
+}
+
+func NewThread() *Thread {
+	return &Thread{
+		//newStack()函数创建Stack结构体实例，参数表示要创建的Stack最多可以容纳多少帧
+		stack: newStack(1024),
+	}
+}
+
+func (self *Thread) PC() int {
+	return self.pc
+}
+
+func (self *Thread) SetPC(pc int) {
+	self.pc = pc
+}
+func (self *Thread) PushFrame(frame *Frame) {
+	self.stack.push(frame)
+}
+
+func (self *Thread) PopFrame() *Frame {
+	return self.stack.pop()
+}
+
+func (self *Thread) CurrentFrame() *Frame {
+	return self.stack.top()
+}
