ffdc44e83c666dbbe4a068fa5b81a52fe84f0cb8
李毅恒 <834805011@qq.com>
2021-10-24 12:17:45 +0800

    BIPUSH与SIPUSH

--- ch05/instructions/constants/ipush.go
+++ ch05/instructions/constants/ipush.go
@@ -20,3 +20,11 @@ func (self *BIPUSH) Execute(frame *rtda.Frame) {
 	i := int32(self.val)
 	frame.OperandStack().PushInt(i)
 }
+
+func (self *SIPUSH) FetchOperands(reader *base.BytecodeReader) {
+	self.val = reader.ReadInt16()
+}
+func (self *SIPUSH) Execute(frame *rtda.Frame) {
+	i := int32(self.val)
+	frame.OperandStack().PushInt(i)
+}
