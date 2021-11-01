19b15facdda8c598175281300d9618f9124e9834
李毅恒 <834805011@qq.com>
2021-11-01 18:22:02 +0800

    类和对象

--- ch06/instructions/references/invokevirtual.go
+++ ch06/instructions/references/invokevirtual.go
@@ -20,8 +20,18 @@ func (self *INVOKE_VIRTUAL) Execute(frame *rtda.Frame) {
 		case "(Z)V":
 			fmt.Printf("%v\n", stack.PopInt() != 0)
 		case "(C)V":
-			z
-
+			fmt.Printf("%c\n", stack.PopInt())
+		case "(I)V", "(B)V", "(S)V":
+			fmt.Printf("%v\n", stack.PopInt())
+		case "(F)V":
+			fmt.Printf("%v\n", stack.PopFloat())
+		case "(J)V":
+			fmt.Printf("%v\n", stack.PopLong())
+		case "(D)V":
+			fmt.Printf("%v\n", stack.PopDouble())
+		default:
+			panic("println: " + methodRef.Descriptor())
 		}
+		stack.PopRef()
 	}
 }
