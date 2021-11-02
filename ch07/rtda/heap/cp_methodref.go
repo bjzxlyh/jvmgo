c2fd70425b9f89e7fa07835811f90398473dfffa
李毅恒 <834805011@qq.com>
2021-11-02 09:29:12 +0800

    解析方法符号引用

--- ch07/rtda/heap/cp_methodref.go
+++ ch07/rtda/heap/cp_methodref.go
@@ -1,6 +1,6 @@
 package heap
 
-import "jvmgo/ch06/classfile"
+import "jvmgo/ch07/classfile"
 
 type MethodRef struct {
 	MemberRef
@@ -12,4 +12,35 @@ func newMethodRef(cp *ConstantPool, refInfo *classfile.ConstantMethodrefInfo) *M
 	ref.cp = cp
 	ref.copyMemberRefInfo(&refInfo.ConstantMemberrefInfo)
 	return ref
+}
+
+func (self *MethodRef) ResolvedMethod() *Method {
+	if self.method == nil {
+		self.resolveMethodRef()
+	}
+	return self.method
+}
+
+func (self *MethodRef) resolveMethodRef() {
+	d := self.cp.class
+	c := self.ResolvedClass()
+	if c.IsInterface() {
+		panic("java.lang.IncompatibleClassChangeError")
+	}
+	method := lookupMethod(c, self.name, self.descriptor)
+	if method == nil {
+		panic("java.lang.NoSuchMethodError")
+	}
+	if !method.isAccessibleTo(d) {
+		panic("java.lang.IllegalAccessError")
+	}
+	self.method = method
+}
+
+func lookupMethod(class *Class, name, descriptor string) *Method {
+	method := LookupMethodInClass(class, name, descriptor)
+	if method == nil {
+		method = lookupMethodInInterfaces(class.interfaces, name, descriptor)
+	}
+	return method
 }
