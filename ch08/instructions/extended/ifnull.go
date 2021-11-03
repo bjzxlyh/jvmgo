aae0f40d274583545e9acd0ddab5014684321bd6
李毅恒 <834805011@qq.com>
2021-11-03 14:17:13 +0800

    数组和字符串

---
+++ ch08/instructions/extended/ifnull.go
@@ -0,0 +1,27 @@
+package extended
+
+import (
+	"jvmgo/ch08/instructions/base"
+	"jvmgo/ch08/rtda"
+)
+
+type IFNULL struct {
+	base.BranchInstruction
+}
+
+type IFNONNULL struct {
+	base.BranchInstruction
+}
+
+func (self *IFNULL) Execute(frame *rtda.Frame) {
+	ref := frame.OperandStack().PopRef()
+	if ref == nil {
+		base.Branch(frame, self.Offset)
+	}
+}
+func (self *IFNONNULL) Execute(frame *rtda.Frame) {
+	ref := frame.OperandStack().PopRef()
+	if ref != nil {
+		base.Branch(frame, self.Offset)
+	}
+}
---
+++ ch08/rtda/thread.go
@@ -0,0 +1,46 @@
+package rtda
+
+import "jvmgo/ch08/rtda/heap"
+
+type Thread struct {
+	pc    int
+	stack *Stack
+}
+
+func NewThread() *Thread {
+	return &Thread{
+		//newStack()函数创建Stack结构体实例，参数表示要创建的Stack最多可以容纳多少帧
+		stack: newStack(1024),
+	}
+}
+
+func (self *Thread) PC() int {
+	return self.pc
+}
+
+func (self *Thread) SetPC(pc int) {
+	self.pc = pc
+}
+func (self *Thread) PushFrame(frame *Frame) {
+	self.stack.push(frame)
+}
+
+func (self *Thread) PopFrame() *Frame {
+	return self.stack.pop()
+}
+
+func (self *Thread) CurrentFrame() *Frame {
+	return self.stack.top()
+}
+
+func (self *Thread) NewFrame(method *heap.Method) *Frame {
+	return newFrame(self, method)
+}
+
+func (self *Thread) TopFrame() *Frame {
+	return self.stack.top()
+}
+
+func (self *Thread) IsStackEmpty() bool {
+	return self.stack.isEmpty()
+}
