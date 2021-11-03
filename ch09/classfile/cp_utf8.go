fed12b22fcdf8116ca83c54f2a32b1f90f8bfede
李毅恒 <834805011@qq.com>
2021-11-03 17:50:57 +0800

    调用本地方法

---
+++ ch09/classfile/cp_utf8.go
@@ -0,0 +1,18 @@
+package classfile
+
+type ConstantUtf8Info struct {
+	str string
+}
+
+/*
+readInfo（）方法先读取出[]byte，然后调用decodeMUTF8（）函数把它解码成Go字符串
+*/
+func (self *ConstantUtf8Info) readInfo(reader *ClassReader) {
+	length := uint32(reader.readUint16())
+	bytes := reader.readBytes(length)
+	self.str = decodeMUTF8(bytes)
+}
+
+func decodeMUTF8(bytes []byte) string {
+	return string(bytes)
+}
